760fc00c60a41aad9ff9f50b7e01938f63e685f5
nguyen duc trong <trongnd02@gmail.com>
2023-10-05 20:36:15 +0700

    add new paper

---
+++ Final_project/Wrong_word_generator/wrong_word_generator.py
@@ -0,0 +1,52 @@
+import random
+
+
+class wrong_word_generator:
+    def insert_char(self, word: str, words_each_pos: int=10) -> dict:
+        '''
+        Input
+            word: Input word to process
+
+        Output
+            result: dict that key is the original word,
+                    value is the string of wrong words separated by delimiter ', '
+        
+        Example:
+        words_each_pos = 2
+            inp: "hi"
+            out: {"hi": "ahi, zhi, hbi, hci"}
+       '''
+       pass
+
+
+    def remove_char(self, word: str): -> dict:
+        '''
+        Input
+            word: Input word to process
+
+        Output
+            result: dict that key is the original word,
+                    value is the string of wrong words separated by delimiter ', '
+        Example:
+            inp: "hello"
+            out: {"hello": "ello, hllo, helo, hell, hell"}
+       '''
+        pass
+
+
+    def 
+    ohell
+    lohel
+    llohe
+    elloh
+    hello
+
+
+def main() -> None:
+
+
+
+    return None
+
+if __name__ == '__main__':
+    main()
