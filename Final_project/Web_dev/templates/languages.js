074d2e373ecb259c6d285d8b5dbe57bfe44f2b52
Phuongnh28 <haphuongng2812@gmail.com>
2023-10-13 20:34:17 +0700

    update html

---
+++ Final_project/Web_dev/templates/languages.js
@@ -0,0 +1,19 @@
+const languages = [
+  {
+    no: "0",
+    name: "Auto",
+    code: "auto",
+  },
+  {
+    no: "16",
+    name: "English",
+    native: "English",
+    code: "en",
+  },
+  {
+    no: "2",
+    name: "Vietnamese",
+    native: "Tiếng Việt",
+    code: "vi",
+  },
+];
