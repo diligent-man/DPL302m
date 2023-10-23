86144c0ab3eec54c16ab4d9ccb3178fbda180e96
nguyen duc trong <trongnd02@gmail.com>
2023-10-24 00:45:21 +0700

    modify en_preprocessing

--- Final_project/Data_preping/Data_gathering/En/Code/data_preprocessor.py
+++ Final_project/Data_preping/Data_gathering/En/Code/data_preprocessor.py
@@ -1,19 +1,26 @@
+from enum import Flag
 import os
 import re
 import shutil
 
+
 class TextPreprocessor:
     def __init__(self):
-        self.__data_path = None
-        self.__preprocessed_data_path = None
+        self.__data_path = ""
+        self.__preprocessed_data_path = ""
+
 
     def set_data_path(self, path):
         self.__data_path = path
 
+
     def get_data_path(self):
         return self.__data_path
+
+
     def set_preprocessed_data_path(self, path):
         self.__preprocessed_data_path = path
+
 
     def get_preprocessed_data_path(self):
         return self.__preprocessed_data_path
@@ -26,11 +33,13 @@ class TextPreprocessor:
             initial_bytes = f.read(2)
         return initial_bytes in [b'\xFE\xFF', b'\xFF\xFE']
 
+
     @staticmethod
     def save_preprocessed_data(preprocessed_data_path: str, filename: str, data: str) -> None:
         with open(preprocessed_data_path + '/' + filename, 'w', encoding='utf-8') as f:
             f.write(data + '\n')
         return None
+
 
     def split_into_sentence(self):
         for filename in os.listdir(self.__data_path):
@@ -45,8 +54,8 @@ class TextPreprocessor:
 
             # save data to preprocessed_data folder
             data = '\n'.join(sentences)
-
             self.save_preprocessed_data(self.__preprocessed_data_path, filename, data)
+
 
     def remove_special_chars(self):
         for filename in sorted(os.listdir(self.__data_path)):
@@ -71,15 +80,30 @@ class TextPreprocessor:
 
 
 def en_preprocessing() -> None:
-    category_ls = ['Business', 'Entertainment', 'Health', 'Sport', 'Style',
-                'Tech', 'Ted_talk', 'Travel', 'Weather',
-                'Wiki','World']
-    data_dir = 'D:/FPT/data/Data'
-    preprocessed_data_dir = "D:/FPT/data/Preprocessed_data"
+    metadata = {"category_ls_1": ['Business', 'Entertainment', 'Health', 'Sport', 'Style', 'Tech', 'Travel', 'Weather','World'],
+                "category_ls_2": ['Ted_talk', 'Wiki'],
+                "data_dir_1": ['../Data/BBC'],
+                "data_dir_2": ['../Data'],
+                "preprocessed_data_dir_1":["../Preprocessed_data/BBC"],
+                "preprocessed_data_dir_2":["../Preprocessed_data"]
+                     }
 
     # split into singular sentence
-    for category in category_ls:
+    index_1 = index_2 = 0
+    for i in range(len(metadata["category_ls_1"]) + len(metadata["category_ls_2"])):
         text_preprocessor = TextPreprocessor()
+
+        # choosing what category will be processed
+        if i < len(metadata["category_ls_1"]):
+            flag = "1"
+            category = metadata["category_ls_" + flag][index_1]
+            index_1 += 1
+        else:
+            flag = "2"
+            category = metadata["category_ls_" + flag][index_2]
+            index_2 += 1
+        data_dir = metadata["data_dir_" + flag][0]
+        preprocessed_data_dir = metadata["preprocessed_data_dir_" + flag][0]
         text_preprocessor.set_data_path(data_dir + '/' + category)
         text_preprocessor.set_preprocessed_data_path(preprocessed_data_dir + '/' + category)
 
@@ -92,12 +116,12 @@ def en_preprocessing() -> None:
         text_preprocessor.split_into_sentence()
 
 
-    # remove punctuation marks
-    for category in category_ls:
-        text_preprocessor = TextPreprocessor()
-        text_preprocessor.set_data_path(preprocessed_data_dir + '/' + category)
-        text_preprocessor.set_preprocessed_data_path(preprocessed_data_dir + '/' + category)
-        text_preprocessor.remove_special_chars()
+    # # remove punctuation marks
+    # for category in category_ls:
+    #     text_preprocessor = TextPreprocessor()
+    #     text_preprocessor.set_data_path(preprocessed_data_dir + '/' + category)
+    #     text_preprocessor.set_preprocessed_data_path(preprocessed_data_dir + '/' + category)
+    #     text_preprocessor.remove_special_chars()
 
 
 def main() -> None:
