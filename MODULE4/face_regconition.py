de2ce1119efe78b62cbae8e0855b4d423154d629
Phuongnh28 <145571634+Phuongnh28@users.noreply.github.com>
2023-10-24 16:42:20 +0700

    Add files via upload

---
+++ MODULE4/face_regconition.py
@@ -0,0 +1,157 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, ZeroPadding2D, Activation, Input, concatenate
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import MaxPooling2D, AveragePooling2D
+from tensorflow.keras.layers import Concatenate
+from tensorflow.keras.layers import Lambda, Flatten, Dense
+from tensorflow.keras.initializers import glorot_uniform
+from tensorflow.keras.layers import Layer
+from tensorflow.keras import backend as K
+K.set_image_data_format('channels_last')
+import os
+import numpy as np
+from numpy import genfromtxt
+import pandas as pd
+import tensorflow as tf
+import PIL
+
+from tensorflow.keras.models import model_from_json
+json_file = open('D:/FU_FA23/DPL302m/LAB/MODULE4/keras-facenet-h5/model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+model = model_from_json(loaded_model_json)
+model.load_weights('D:/FU_FA23/DPL302m/LAB/MODULE4/keras-facenet-h5/model.h5')
+
+print(model.inputs)
+print(model.outputs)
+
+def triplet_loss(y_true, y_pred, alpha = 0.2):
+    anchor, positive, negative = y_pred[0], y_pred[1], y_pred[2]
+    # Step 1: Compute the (encoding) distance between the anchor and the positive
+    pos_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, positive)), axis=-1)
+    # Step 2: Compute the (encoding) distance between the anchor and the negative
+    neg_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, negative)), axis=-1)
+    # Step 3: subtract the two previous distances and add alpha.
+    basic_loss = tf.add(tf.subtract(pos_dist, neg_dist), alpha)
+    # Step 4: Take the maximum of basic_loss and 0.0. Sum over the training examples.
+    loss = tf.reduce_sum(tf.maximum(basic_loss, 0.0))
+    return loss
+
+tf.random.set_seed(1)
+y_true = (None, None, None) # It is not used
+y_pred = (tf.keras.backend.random_normal([3, 128], mean=6, stddev=0.1, seed = 1),
+        tf.keras.backend.random_normal([3, 128], mean=1, stddev=1, seed = 1),
+        tf.keras.backend.random_normal([3, 128], mean=3, stddev=4, seed = 1))
+loss = triplet_loss(y_true, y_pred)
+
+assert type(loss) == tf.python.framework.ops.EagerTensor, "Use tensorflow functions"
+print("loss = " + str(loss))
+
+y_pred_perfect = ([1., 1.], [1., 1.], [1., 1.,])
+loss = triplet_loss(y_true, y_pred_perfect, 5)
+assert loss == 5, "Wrong value. Did you add the alpha to basic_loss?"
+y_pred_perfect = ([1., 1.],[1., 1.], [0., 0.,])
+loss = triplet_loss(y_true, y_pred_perfect, 3)
+assert loss == 1., "Wrong value. Check that pos_dist = 0 and neg_dist = 2 in this example"
+y_pred_perfect = ([1., 1.],[0., 0.], [1., 1.,])
+loss = triplet_loss(y_true, y_pred_perfect, 0)
+assert loss == 2., "Wrong value. Check that pos_dist = 2 and neg_dist = 0 in this example"
+y_pred_perfect = ([0., 0.],[0., 0.], [0., 0.,])
+loss = triplet_loss(y_true, y_pred_perfect, -2)
+assert loss == 0, "Wrong value. Are you taking the maximum between basic_loss and 0?"
+y_pred_perfect = ([[1., 0.], [1., 0.]],[[1., 0.], [1., 0.]], [[0., 1.], [0., 1.]])
+loss = triplet_loss(y_true, y_pred_perfect, 3)
+assert loss == 2., "Wrong value. Are you applying tf.reduce_sum to get the loss?"
+y_pred_perfect = ([[1., 1.], [2., 0.]], [[0., 3.], [1., 1.]], [[1., 0.], [0., 1.,]])
+loss = triplet_loss(y_true, y_pred_perfect, 1)
+if (loss == 4.):
+    raise Exception('Perhaps you are not using axis=-1 in reduce_sum?')
+assert loss == 5, "Wrong value. Check your implementation"
+
+FRmodel = model
+#tf.keras.backend.set_image_data_format('channels_last')
+def img_to_encoding(image_path, model):
+    img = tf.keras.preprocessing.image.load_img(image_path, target_size=(160, 160))
+    img = np.around(np.array(img) / 255.0, decimals=12)
+    x_train = np.expand_dims(img, axis=0)
+    embedding = model.predict_on_batch(x_train)
+    return embedding / np.linalg.norm(embedding, ord=2)
+database = {}
+database["danielle"] = img_to_encoding("images/danielle.png", FRmodel)
+database["younes"] = img_to_encoding("images/younes.jpg", FRmodel)
+database["tian"] = img_to_encoding("images/tian.jpg", FRmodel)
+database["andrew"] = img_to_encoding("images/andrew.jpg", FRmodel)
+database["kian"] = img_to_encoding("images/kian.jpg", FRmodel)
+database["dan"] = img_to_encoding("images/dan.jpg", FRmodel)
+database["sebastiano"] = img_to_encoding("images/sebastiano.jpg", FRmodel)
+database["bertrand"] = img_to_encoding("images/bertrand.jpg", FRmodel)
+database["kevin"] = img_to_encoding("images/kevin.jpg", FRmodel)
+database["felix"] = img_to_encoding("images/felix.jpg", FRmodel)
+database["benoit"] = img_to_encoding("images/benoit.jpg", FRmodel)
+database["arnaud"] = img_to_encoding("images/arnaud.jpg", FRmodel)
+
+danielle = tf.keras.preprocessing.image.load_img("images/danielle.png", target_size=(160, 160))
+kian = tf.keras.preprocessing.image.load_img("images/kian.jpg", target_size=(160, 160))
+
+np.around(np.array(kian) / 255.0, decimals=12).shape
+print("kian")
+np.around(np.array(danielle) / 255.0, decimals=12).shape
+print("danielle")
+def verify(image_path, identity, database, model):
+    # Step 1: Compute the encoding for the image. Use img_to_encoding() see example above. (≈ 1 line)
+    encoding = img_to_encoding(image_path, model)
+    # Step 2: Compute distance with identity's image (≈ 1 line)
+    dist = np.linalg.norm(encoding - database[identity])
+    # Step 3: Open the door if dist < 0.7, else don't open (≈ 3 lines)
+    if dist < 0.7:
+        print("It's " + str(identity) + ", welcome in!")
+        door_open = True
+    else:
+        print("It's not " + str(identity) + ", please go away")
+        door_open = False      
+    return dist, door_open
+assert(np.allclose(verify("images/camera_1.jpg", "bertrand", database, FRmodel), (0.54364836, True)))
+assert(np.allclose(verify("images/camera_3.jpg", "bertrand", database, FRmodel), (0.38616243, True)))
+assert(np.allclose(verify("images/camera_1.jpg", "younes", database, FRmodel), (1.3963861, False)))
+assert(np.allclose(verify("images/camera_3.jpg", "younes", database, FRmodel), (1.3872949, False)))
+
+verify("images/camera_0.jpg", "younes", database, FRmodel)
+verify("images/camera_2.jpg", "kian", database, FRmodel)
+
+
+def who_is_it(image_path, database, model):
+## Step 1: Compute the target "encoding" for the image. Use img_to_encoding() see example above. ## (≈ 1 line)
+    encoding =  img_to_encoding(image_path, model)
+    
+    ## Step 2: Find the closest encoding ##
+    min_dist = 100
+    
+    # Loop over the database dictionary's names and encodings.
+    for (name, db_enc) in database.items():
+        
+        # Compute L2 distance between the target "encoding" and the current db_enc from the database. 
+        dist = np.linalg.norm(encoding - db_enc)
+
+        # If this distance is less than the min_dist, then set min_dist to dist, and identity to name. 
+        if dist < min_dist:
+            min_dist = dist
+            identity = name
+    if min_dist > 0.7:
+        print("Not in the database.")
+    else:
+        print ("it's " + str(identity) + ", the distance is " + str(min_dist))
+        
+    return min_dist, identity
+# Test 1 with Younes pictures 
+who_is_it("images/camera_0.jpg", database, FRmodel)
+
+# Test 2 with Younes pictures 
+test1 = who_is_it("images/camera_0.jpg", database, FRmodel)
+assert np.isclose(test1[0], 0.5992946)
+assert test1[1] == 'younes'
+
+# Test 3 with Younes pictures 
+test2 = who_is_it("images/younes.jpg", database, FRmodel)
+assert np.isclose(test2[0], 0.0)
+assert test2[1] == 'younes'
