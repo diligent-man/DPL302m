af98dc76fe0331533305a20994ad8428f7c09fde
nguyen duc trong <trongnd02@gmail.com>
2023-10-25 15:03:25 +0700

    add data_generation

---
+++ Final_project/Data_preping/Data_gathering/En/Data_generation/data_generation.py
@@ -0,0 +1,35 @@
+
+def sentence_generation(sentence: str, num_of_wrong_sentence: int) -> None:
+    """
+    Input: single sentence
+           num_of_wrong_sentence: how many wrong sentences are generated
+    
+    Output: pair of correct & incorrect sentence
+
+    Example:
+        inp: correct sentence
+        out: correct sentence|incorrect sentence_1
+             correct sentence|incorrect sentence_2
+             correct sentence|incorrect sentence_3
+             correct sentence|incorrect sentence_4
+             correct sentence|incorrect sentence_5
+    
+    Constraints:
+        len(sen) > 20: sai 5 words
+        len(sen) > 10: sai 2 words
+
+    Naming convention:
+        sentence_0.txt
+        sentence_1.txt
+        sentence_2.txt
+    """
+    
+    return None
+
+def main() -> None:
+    sentence_generation()
+    return None
+
+
+if __name__ == '__main__':
+    main()
